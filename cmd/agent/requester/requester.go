43ee16a234b8c62efecc9cb228c10c4d5ce83c33
Nikita Ivanov <nivanov045@yandex-team.ru>
2022-07-24 19:14:52 +0700

    increment-4: Fixed potential problem.

--- cmd/agent/requester/requester.go
+++ cmd/agent/requester/requester.go
@@ -16,13 +16,13 @@ func (*requester) Send(a []byte) error {
 		fmt.Println("err != nil")
 		return err
 	}
-	request.Header.Add("Content-Type", "application/json")
+	request.Header.Set("Content-Type", "application/json")
 	request.Close = true
-	response, err := client.Do(request)
 	fmt.Println(request.URL)
 	fmt.Println(string(a))
+	response, err := client.Do(request)
 	if err != nil {
-		fmt.Println("err != nil 2")
+		fmt.Println("err != nil 2 with ", err)
 		return err
 	}
 	defer response.Body.Close()
