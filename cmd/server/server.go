55cd49c0c06c1f6f23878bca93415a5a1dd6d19e
Nikita Ivanov <nivanov045@yandex-team.ru>
2022-07-30 14:26:12 +0700

    increment-7: Typo was fixed.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -13,7 +13,7 @@ import (
 
 type config struct {
 	Address       string        `env:"ADDRESS" envDefault:"127.0.0.1:8080"`
-	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"0s"`
+	StoreInterval time.Duration `env:"STORE_INTERVAL" envDefault:"300s"`
 	StoreFile     string        `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
 	Restore       bool          `env:"RESTORE" envDefault:"true"`
 }
