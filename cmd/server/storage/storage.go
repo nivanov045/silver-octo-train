3391dcf062a19a9faa6ca03d2b61e4dc56b4b51d
Nikita Ivanov <nivanov045@yandex-team.ru>
2022-07-30 12:37:45 +0700

    increment-6: Save data to file was added.

--- cmd/server/storage/storage.go
+++ cmd/server/storage/storage.go
@@ -1,15 +1,25 @@
 package storage
 
 import (
+	"encoding/json"
 	"github.com/nivanov045/silver-octo-train/internal/metrics"
+	"log"
+	"os"
+	"runtime"
+	"time"
 )
 
 type storage struct {
-	Metrics metrics.Metrics
+	Metrics       metrics.Metrics
+	storeInterval time.Duration
+	storeFile     string
+	restore       bool
+	hasUpdates    bool
 }
 
 func (s *storage) SetCounterMetrics(name string, val metrics.Counter) {
 	s.Metrics.CounterMetrics[name] = val
+	s.hasUpdates = true
 }
 
 func (s *storage) GetCounterMetrics(name string) (metrics.Counter, bool) {
@@ -21,6 +31,7 @@ func (s *storage) GetCounterMetrics(name string) (metrics.Counter, bool) {
 
 func (s *storage) SetGaugeMetrics(name string, val metrics.Gauge) {
 	s.Metrics.GaugeMetrics[name] = val
+	s.hasUpdates = true
 }
 
 func (s *storage) GetGaugeMetrics(name string) (metrics.Gauge, bool) {
@@ -41,11 +52,82 @@ func (s *storage) GetKnownMetrics() []string {
 	return res
 }
 
-func New() *storage {
-	return &storage{
+func (s *storage) restoreFromFile() error {
+	file, err := os.OpenFile(s.storeFile, os.O_RDONLY|os.O_CREATE, 0777)
+	if err != nil {
+		log.Println("storage::restoreFromFile: can't open file:", err)
+		return err
+	}
+	defer file.Close()
+	encoder := json.NewDecoder(file)
+	err = encoder.Decode(&s.Metrics)
+	if err != nil {
+		log.Println("storage::restoreFromFile: can't read from file:", err)
+		return err
+	}
+	return nil
+}
+
+func (s *storage) saveToFile() error {
+	log.Println("storage::saveToFile: started")
+	if !s.hasUpdates {
+		log.Println("storage::saveToFile: nothing to update")
+		return nil
+	}
+	file, err := os.OpenFile(s.storeFile, os.O_WRONLY|os.O_CREATE, 0777)
+	if err != nil {
+		log.Println("storage::saveToFile: can't open file:", err)
+		return err
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(&s.Metrics)
+	if err != nil {
+		log.Println("storage::saveToFile: can't write to file:", err)
+		return err
+	}
+	s.hasUpdates = false
+	return nil
+}
+
+func StorageFinalizer(s *storage) {
+	log.Println("storage::StorageFinalizer: started")
+	err := s.saveToFile()
+	if err != nil {
+		log.Println("storage::StorageFinalizer: can't make final save to file:", err)
+	}
+}
+
+func (s *storage) saveByTimer() {
+	ticker := time.NewTicker(s.storeInterval)
+	for {
+		<-ticker.C
+		log.Println("storage::saveByTimer: ticker")
+		err := s.saveToFile()
+		if err != nil {
+			log.Println("storage::saveByTimer: can't make usual save to file:", err)
+		}
+	}
+}
+
+func New(storeInterval time.Duration, storeFile string, restore bool) *storage {
+	var res = &storage{
 		Metrics: metrics.Metrics{
 			GaugeMetrics:   map[string]metrics.Gauge{},
 			CounterMetrics: map[string]metrics.Counter{},
 		},
+		storeInterval: storeInterval,
+		storeFile:     storeFile,
+		restore:       restore,
+		hasUpdates:    false,
 	}
+	if restore {
+		err := res.restoreFromFile()
+		if err != nil {
+			log.Println("storage::New: restore error:", err)
+		}
+	}
+	runtime.SetFinalizer(res, StorageFinalizer)
+	go res.saveByTimer()
+	return res
 }
