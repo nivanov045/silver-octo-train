150607e9b0f9da29cf8efe32456e1aa7dc2bedc9
Nikita Ivanov <nivanov045@yandex-team.ru>
2022-08-03 10:14:57 +0700

    sprint-2: Ability to finish gorutine using global context was realised.

--- cmd/agent/metricsagent/metricsagent.go
+++ cmd/agent/metricsagent/metricsagent.go
@@ -1,6 +1,7 @@
 package metricsagent
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/nivanov045/silver-octo-train/cmd/agent/agentconfig"
 	"log"
@@ -27,58 +28,70 @@ func New(c agentconfig.Config) *metricsagent {
 
 func (a *metricsagent) updateMetrics() {
 	ticker := time.NewTicker(a.config.PollInterval)
+	ctx := context.Background()
 	for {
-		<-ticker.C
-		log.Println("metricsagent::updateMetrics: time to update")
-		m := <-a.metricsChannel
-		a.metricsChannel <- m
-		metricsperformer.New().UpdateMetrics(m)
-		<-a.metricsChannel
-		a.metricsChannel <- m
-		log.Println("metricsagent::updateMetrics: metrics were updated")
+		select {
+		case <-ctx.Done():
+			log.Println("metricsagent::updateMetrics: ctx.Done")
+			return
+		case <-ticker.C:
+			log.Println("metricsagent::updateMetrics: time to update")
+			m := <-a.metricsChannel
+			a.metricsChannel <- m
+			metricsperformer.New().UpdateMetrics(m)
+			<-a.metricsChannel
+			a.metricsChannel <- m
+			log.Println("metricsagent::updateMetrics: metrics were updated")
+		}
 	}
 }
 
 func (a *metricsagent) sendMetrics() {
 	ticker := time.NewTicker(a.config.ReportInterval)
+	ctx := context.Background()
 	for {
-		<-ticker.C
-		m := <-a.metricsChannel
-		a.metricsChannel <- m
-		for key, val := range m.GaugeMetrics {
-			asFloat := float64(val)
+		select {
+		case <-ctx.Done():
+			log.Println("metricsagent::sendMetrics: ctx.Done")
+			return
+		case <-ticker.C:
+			m := <-a.metricsChannel
+			a.metricsChannel <- m
+			for key, val := range m.GaugeMetrics {
+				asFloat := float64(val)
+				metricForSend := metrics.MetricsInterface{
+					ID:    key,
+					MType: "gauge",
+					Delta: nil,
+					Value: &asFloat,
+				}
+				marshalled, err := json.Marshal(metricForSend)
+				if err != nil {
+					log.Panicln("metricsagent::sendMetrics: can't marshal gauge metric for sand with", err)
+				}
+				err = a.requester.Send(marshalled)
+				if err != nil {
+					log.Println("metricsagent::sendMetrics: can't send gauge with", err)
+				}
+			}
+			pc := m.CounterMetrics["PollCount"]
+			asint := int64(pc)
 			metricForSend := metrics.MetricsInterface{
-				ID:    key,
-				MType: "gauge",
-				Delta: nil,
-				Value: &asFloat,
+				ID:    "PollCount",
+				MType: "counter",
+				Delta: &asint,
+				Value: nil,
 			}
 			marshalled, err := json.Marshal(metricForSend)
 			if err != nil {
-				log.Panicln("metricsagent::sendMetrics: can't marshal gauge metric for sand with", err)
+				log.Panicln("metricsagent::sendMetrics: can't marshal PollCount metric for sand with", err)
 			}
 			err = a.requester.Send(marshalled)
 			if err != nil {
-				log.Println("metricsagent::sendMetrics: can't send gauge with", err)
+				log.Println("metricsagent::sendMetrics: can't send PollCount with", err)
 			}
+			log.Println("metricsagent::sendMetrics: metrics were sent")
 		}
-		pc := m.CounterMetrics["PollCount"]
-		asint := int64(pc)
-		metricForSend := metrics.MetricsInterface{
-			ID:    "PollCount",
-			MType: "counter",
-			Delta: &asint,
-			Value: nil,
-		}
-		marshalled, err := json.Marshal(metricForSend)
-		if err != nil {
-			log.Panicln("metricsagent::sendMetrics: can't marshal PollCount metric for sand with", err)
-		}
-		err = a.requester.Send(marshalled)
-		if err != nil {
-			log.Println("metricsagent::sendMetrics: can't send PollCount with", err)
-		}
-		log.Println("metricsagent::sendMetrics: metrics were sent")
 	}
 }
 
